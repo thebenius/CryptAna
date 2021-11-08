3aebb852846db45fb6b53f3adfc63de1eea4bc2e
thebenius <bennyhaag@protonmail.com>
2021-11-08 18:39:50 +0100

    added caesar example

---
+++ caesar.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+from classes.CAESAR import CAESAR
+from sys import stdin
+
+'''
+    Reads a CAESAR cipher from the stdin and bruteforces it against the default alphabeth
+'''
+
+
+print("Please select the mode:")
+print("encrypt (e)")
+print("decrypt (d)")
+print("reverse (r)")
+print("mode: ", end='')
+mode = list(input())[0]
+if (mode != 'e' and mode != 'd' and mode != 'r'):
+  print("invalid mode")
+  exit()
+
+
+caesar = CAESAR()
+text = ""
+key = 0
+
+if (mode == 'r'):
+  print("Cipher:")
+
+else:
+  print("key: ", end='')
+  key = int(input())
+
+  if (mode == 'e'):
+    print("Plaintext:")
+  if (mode == 'd'):
+    print("Cipher")
+
+
+for line in stdin:
+  text += line
+
+if (mode == 'r'):
+  caesar.setCipher(text)
+  print("\n\nPossible Solutions:")
+  for key in  range(len(caesar.getAlphabeth())):
+      caesar.setKey(key)
+      print(f"{key}: {caesar.decrypt()}")
+  exit()
+
+caesar.setKey(key)
+
+if (mode == 'e'):
+  caesar.setText(text)
+  print(caesar.encrypt())
+  
+if (mode == 'd'):
+  caesar.setCipher(text)
+  print(caesar.decrypt())
+
+
+
+
